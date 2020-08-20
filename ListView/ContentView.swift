76d96f1389e0aa91a5d0293f2eafdf4d8b35f580
Aiur <gangrue@gmail.com>
2020-08-20 21:28:49 +0800

    Initial Commit

---
+++ ListView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListView
+//
+//  Created by Aiur on 20.08.2020.
+//  Copyright © 2020 Aiur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
